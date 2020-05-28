f715d40a2203a0f0a60dfd89b709d0784494d153
RenanLDalmeida <rdalmeidi@gmail.com>
2020-05-28 15:23:35 -0300

    Instancia finalizada

--- Celular.cs
+++ Celular.cs
@@ -14,21 +14,36 @@ namespace CelularPOO
 
         //Métodos
 
-
+        /// <summary>
+        /// Ligar o celular
+        /// </summary>
+        /// <returns>Mensagem de ligando celular</returns>
         public string Ligar(){
             ligado = true;
             return "Ligando celular";
         }
 
+        /// <summary>
+        /// Desligar o celular
+        /// </summary>
+        /// <returns>Mensagem de desligando celular</returns>
         public string Desligar(){
             ligado = false;
             return "Desligando celular";
         }
 
+        /// <summary>
+        /// Fazer ligação
+        /// </summary>
+        /// <returns>Mensagem de fazendo ligação</returns>
         public string Fazerligacao(){
             return "Fazendo ligação";
         }
-
+        
+        /// <summary>
+        /// Enviar mensagem
+        /// </summary>
+        /// <returns>Mensagem de enviando mensagem</returns>
         public string EnviarMensagem(){
             return "Enviando mensagem";
         }
--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace CelularPOO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Celular android = new Celular();
+
+            android.modelo = "S10";
+            android.tamanho = 6.4f;
+            android.cor = "Preto";
+            android.ligado = false;
+
+            if (android.ligado){
+            
+            Console.WriteLine( android.EnviarMensagem() );
+            Console.WriteLine( android.Fazerligacao() );
+            }
+            else{
+            Console.WriteLine("O celular está desligado.");
+
+            }
         }
     }
 }
