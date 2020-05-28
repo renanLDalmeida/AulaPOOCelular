aeeabc08cfa1aaafa7099639ef0b4ac72aecaadc
RenanLDalmeida <rdalmeidi@gmail.com>
2020-05-28 15:09:38 -0300

    Atributos e Métodos finalizados

--- Celular.cs
+++ Celular.cs
@@ -2,6 +2,36 @@ namespace CelularPOO
 {
     public class Celular
     {
-        
+        // Atributos
+        public string cor;
+
+        public string modelo;
+
+        public float tamanho;
+
+        public bool ligado;
+
+
+        //Métodos
+
+
+        public string Ligar(){
+            ligado = true;
+            return "Ligando celular";
+        }
+
+        public string Desligar(){
+            ligado = false;
+            return "Desligando celular";
+        }
+
+        public string Fazerligacao(){
+            return "Fazendo ligação";
+        }
+
+        public string EnviarMensagem(){
+            return "Enviando mensagem";
+        }
+
     }
 }
